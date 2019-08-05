a7830dfd0b37293bac22cf5d61cc21fed150dd3b
Icepo <104494108@qq.com>
2019-08-05 19:38:45 +0800

    init

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 from worker import Worker
 
 if __name__ == '__main__':
-    max_thread = 3  # 测试线程数量
-
-    threads = [lambda: [Worker("t" + str(i)) for i in range(max_thread)]][0]()  # 线程列表
-
-    [t.start() for t in threads]    # 启动线程
-
-    [t.join() for t in threads]     # 等待子线程结束 但是子线程里面是死循环 所以你懂的
+    _ = [th for th in [t for t in [lambda: [Worker("t_" + str(i)) for i in range(3)]][0]() if
+                       not t.setDaemon(True) and not t.start()] if
+         not th.join()]
